22a302aecd407cce6d9e5fa1e2599f887a1bcf9f
Josh Gillies <github@joshgilli.es>
2015-03-17 13:08:41 +1100

    We have lift-off! :rocket:

--- README.md
+++ README.md
@@ -1,2 +1,86 @@
 # node-matrix-import-actions
-Action specification for the Squiz Matrix "Import Assets from XML Tool".
+
+Action specification for Squiz Matrix' "[Import Assets from XML Tool]".
+
+[![Build Status](https://travis-ci.org/joshgillies/node-matrix-import-actions.svg)](https://travis-ci.org/joshgillies/node-matrix-import-actions)
+
+## Example
+
+```js
+var Action = require('node-matrix-import-actions');
+
+var createAction = new Action('create_asset', {
+  id: 'Site_1',
+  parentId: 1,
+  type: 'site'
+});
+
+```
+
+## API
+
+### Actions
+
+An action is a task performed against a Squiz Matrix system.
+
+`node-matrix-import-actions` provides an Action constructor via `require('node-matrix-import-actions')`
+
+#### Action(type, opts)
+
+`type` is a String representing the type of action. The following types are available:
+
+#### Action('add_path', opts)
+
+A `add_path` Action accepts an Object as it's second argument `opts` with the following properties:
+
+  * `opts.id`
+  * `opts.path`
+  * `opts.assetId`
+
+#### Action('create_asset', opts)
+
+A `create_asset` Action accepts an Object as it's second argument `opts` with the following properties:
+
+  * `opts.id`
+  * `opts.parentId`
+  * `opts.type`
+  * `opts.link`
+  * `opts.value`
+  * `opts.dependant`
+  * `opts.exclusive`
+
+#### Action('create_link', opts)
+
+A `create_link` Action accepts an Object as it's second argument `opts` with the following properties:
+
+  * `opts.to`
+  * `opts.from`
+  * `opts.link`
+  * `opts.value`
+  * `opts.dependant`
+  * `opts.exclusive`
+  * `opts.major`
+
+#### Action('set_attribute', opts)
+
+A `set_attribute` Action accepts an Object as it's second argument `opts` with the following properties:
+
+  * `opts.id`
+  * `opts.assetId`
+  * `opts.attribute`
+  * `opts.value`
+
+#### Action('set_permission', opts)
+
+A `set_permission` Action accepts an Object as it's second argument `opts` with the following properties:
+
+  * `opts.assetId`
+  * `opts.permission`
+  * `opts.granted`
+  * `opts.userId`
+
+## License
+
+MIT
+
+[Import Assets from XML Tool]: http://manuals.matrix.squizsuite.net/tools/chapters/import-assets-from-xml-tool
---
+++ test.js
@@ -0,0 +1,116 @@
+var helpers = require('./helpers');
+var action = require('./');
+var test = require('tape');
+var tests = {
+  add_path: {
+    opts: {
+      id: 'Site_1',
+      path: 'test-site',
+      assetId: '1'
+    },
+    expected: {
+      action_id: 'add_Site_1_path',
+      action_type: 'add_web_path',
+      asset: '1',
+      path: 'test-site'
+    }
+  },
+  create_asset: {
+    opts: {
+      id: 'Site_1',
+      parentId: 1,
+      type: 'site'
+    },
+    expected: {
+      action_id: 'create_Site_1',
+      action_type: 'create_asset',
+      type_code: 'site',
+      parentid: 1,
+      value: '',
+      link_type: 1,
+      is_dependant: 0,
+      is_exclusive: 0
+    }
+  },
+  create_link: {
+    opts: {
+      to: 2,
+      from: 1
+    },
+    expected: {
+      action_id: 'link_1_2_to_1',
+      action_type: 'create_link',
+      asset: 1,
+      assetid: 2,
+      is_dependant: 0,
+      is_exclusive: 0,
+      is_major: 0,
+      link_type: 1,
+      value: ''
+    }
+  },
+  set_attribute: {
+    opts: {
+      id: 'Site_1',
+      assetId: 1,
+      attribute: 'html',
+      value: 'Test Site'
+    },
+    expected: {
+      action_id: 'set_html_Site_1',
+      action_type: 'set_attribute_value',
+      asset: 1,
+      attribute: 'html',
+      value: 'Test Site'
+    }
+  },
+  set_permission: {
+    opts: {
+      assetId: 1,
+      permission: 'read',
+      granted: true,
+      userId: 7
+    },
+    expected: {
+      action_id: 'set_permission_1_read_7',
+      action_type: 'set_permission',
+      asset: 1,
+      permission: 1,
+      granted: 1,
+      userid: 7
+    }
+  }
+};
+
+test('use shorthand helpers', function(t) {
+  t.equal(helpers.keyShorthand('add_path'), 'add_web_path', 'shorthand selector');
+  t.equal(helpers.keyShorthand('create_asset'), 'create_asset', 'passthrough when no shorthand is available');
+  t.end();
+});
+
+test('join asset type with id', function(t) {
+  t.equal(helpers.asset('Site', '1'), 'Site_1');
+  t.end();
+});
+
+test('create action ids', function(t) {
+  Object.keys(tests).forEach(function(test) {
+    t.deepEqual(
+      (action.Action(test, tests[test].opts)).action_id,
+      tests[test].expected.action_id,
+      test + ' action id'
+    );
+  });
+  t.end();
+});
+
+test('create actions', function(t) {
+  Object.keys(tests).forEach(function(test) {
+    t.deepEqual(
+      action.Action(test, tests[test].opts),
+      tests[test].expected,
+      'action \'' + test + '\' from Constructor object'
+    );
+  });
+  t.end();
+});
